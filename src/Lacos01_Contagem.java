b7aa5eee63311e22c8014c500983dbb497795ef6
Andie <andrielle93425@gmail.com>
2026-09-15 00:00:09 -0300

    Adiciona novos exercícios

---
+++ src/Lacos01_Contagem.java
@@ -0,0 +1,14 @@
+/*
+Exercício 1 – Contagem de 1 a 10
+Faça um programa que exiba os números de 1 até 10 usando for.
+ */
+
+public class Lacos01_Contagem {
+
+	public static void main(String[] args) {
+		
+		for (int i = 1; i <= 10; i++) System.out.println(i);
+
+	}
+
+}
